ad3e8e657749d851a76c59cc44c8ae8e0a191fc2
AhmedBakrXI <ahmedbakr2501@gmail.com>
2025-08-10 11:38:49 +0300

    Add validation annotations to AuthenticationRequest and introduce banner file

--- src/main/java/org/project/ecommerce/user/dto/auth/request/AuthenticationRequest.java
+++ src/main/java/org/project/ecommerce/user/dto/auth/request/AuthenticationRequest.java
@@ -1,5 +1,7 @@
 package org.project.ecommerce.user.dto.auth.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,7 +12,27 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AuthenticationRequest {
+    @NotNull
+    @NotBlank(message = "username cannot be blank")
+    @Size(
+            min = 5,
+            max = 50
+    )
+    @Pattern(
+            regexp = "^\\p{Nd}"
+    )
+    @Schema(
+            name = "Username",
+            example = "Ahmed",
+            minimum = "5 Letters and numbers",
+            maximum = "50 Letters and numbers"
+    )
     private String username;
+
+    @NotNull
     private String password;
+
+    @Email
+    @NotNull
     private String email;
 }
